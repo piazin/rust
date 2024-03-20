7791a47a8c01fbde7f32770ee58fa9eb2ce56aa3
Lucas <ls4803326@gmail.com>
2024-03-20 06:50:28 -0300

    functions t=875

---
+++ src/functions.rs
@@ -0,0 +1,9 @@
+pub fn functions() {
+    say_hello("Lucas");
+}
+
+fn say_hello(name: &str) {
+    println!("Hello {name}");
+}
+
+// https://youtu.be/jNaVT19Pwd0?list=LL&t=875
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,23 @@ mod memories;
 mod primitives_types;
 mod string;
 mod variables;
-mod calculations;
+// mod calculations;
+mod functions;
 
 use memories::memories;
 use primitives_types::primitives_types;
 use string::string;
 use variables::variables;
-use calculations::calculations;
+// use calculations::calculations;
+use functions::functions;
 
 fn main() {
     variables();
     primitives_types();
     memories();
     string();
-    calculations();
+    // calculations();
+    functions();
 
     //primitives_types::primitives_types();
 }
